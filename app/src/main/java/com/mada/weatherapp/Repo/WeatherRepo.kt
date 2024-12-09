f724100e2c1c9030e2c51d48f3c3671261b5935e
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2024-12-09 13:37:42 +0200

    Add data module

---
+++ app/src/main/java/com/mada/weatherapp/Repo/WeatherRepo.kt
@@ -0,0 +1,10 @@
+package com.mada.weatherapp.Repo
+
+import com.mada.weatherapp.Server.ApiServices
+
+class WeatherRepo(val api:ApiServices) {
+
+    fun getCurrentWeatherRepo(lat:Double,lng:Double,unit:String){
+        api.getCurrentWeather(lat,lng,unit,"252fec42e0cfcea3bbb55d25a665e1bf")
+    }
+}
--- app/src/main/java/com/mada/weatherapp/Server/ApiClient.kt
+++ app/src/main/java/com/mada/weatherapp/Server/ApiClient.kt
@@ -1,4 +1,24 @@
 package com.mada.weatherapp.Server
 
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 class ApiClient {
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(60,TimeUnit.SECONDS)
+        .readTimeout(60,TimeUnit.SECONDS)
+        .writeTimeout(60,TimeUnit.SECONDS)
+        .build()
+
+    private lateinit var retrofit: Retrofit
+    fun getClient():Retrofit{
+        retrofit = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org")
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit
+    }
 }
